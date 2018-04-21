e9c46b92ac5f82fb45097bd8bd42ee72d9cf5a91
hafsaMkhan <hafsa.maqsood15@gmail.com>
2018-04-21 16:14:26 +0500

    UserDL function

--- Blogging/UserDL.cs
+++ Blogging/UserDL.cs
@@ -11,21 +11,82 @@ namespace Blogging
 
         public bool AddUser(string user, string pwd, string cpwd, string s_ques, string ans, string gnder)
         {
-           
-     }
-     
+            bool is_valid = false;
+            foreach (User a in UserDL.users)
+            {
+                if (a.username == user)
+                {
+                    is_valid = false;
+                }
+            }
+            if (pwd != cpwd)
+            {
+
+            }
+            else
+            {
+                User u = new User();
+                u.username = user;
+                u.password = pwd;
+                u.cpassword = cpwd;
+                u.secretQuestion = s_ques;
+                u.answer = ans;
+                u.gender = gnder;
+                users.Add(u);
+                is_valid = true;
+            }
+            return is_valid;
+        }
+
         public void Delete()
         {
 
         }
         public bool ResetPassword(string user, string pwd, string cpwd, string s_ques, string ans)
         {
-            
+            bool isvalid = false;
+            foreach (User u in UserDL.users)
+            {
+                if (u.username == user)
+                {
+                    if (u.secretQuestion == s_ques & u.answer == ans)
+                    {
+                        if (pwd == cpwd)
+                        {
+                            u.password = pwd;
+                            u.cpassword = cpwd;
+                            isvalid = true;
+                        }
+                    }
+                    else
+                    {
+                        if (u.secretQuestion != s_ques & u.answer == ans)
+                        {
+                            isvalid = false;
+
+                        }
+                        else if (pwd != cpwd)
+                        {
+                            isvalid = false;
+                        }
+                    }
+                }
+            }
+            return isvalid;
         }
         public bool Login(string user, string pwd)
         {
-            
+            bool valid = false;
+            foreach (User u in UserDL.users)
+            {
+                if (u.username == user & u.password == pwd)
+                {
+                    valid = true;
+                }
+            }
+            return valid;
         }
+
         public void Logout()
         {        }
 
